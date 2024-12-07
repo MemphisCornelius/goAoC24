b998598a674f2e226f4106e565310645bfa3840b
Marvi <accounts.marvi@mailbox.org>
2024-12-07 21:48:51 +0100

    deduplicate code in solution7

--- solutions/Day7/solution.go
+++ solutions/Day7/solution.go
@@ -10,33 +10,37 @@ import (
 
 type Day7 struct{}
 
-func (_ Day7) Run1(s *bufio.Scanner) int {
-	sum := 0
-	for s.Scan() {
-		line := s.Text()
-		resultsplit := strings.Split(line, ": ")
-		result, err := strconv.Atoi(resultsplit[0])
+func parseInput(line string) (int, []int) {
+
+	resultsplit := strings.Split(line, ": ")
+	result, err := strconv.Atoi(resultsplit[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	numbers := strings.Split(resultsplit[1], " ")
+	values := make([]int, len(numbers))
+	for i, v := range numbers {
+		values[i], err = strconv.Atoi(v)
 		if err != nil {
 			log.Fatal(err)
 		}
-		numbers := strings.Split(resultsplit[1], " ")
-		values := make([]int, len(numbers))
-		for i, v := range numbers {
-			values[i], err = strconv.Atoi(v)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+	}
+	return result, values
+}
 
-		if validOperation(result, values) {
+func (_ Day7) Run1(s *bufio.Scanner) int {
+	sum := 0
+	for s.Scan() {
+		result, values := parseInput(s.Text())
+
+		if validOperation(1, result, values) {
 			sum += result
 		}
 	}
-
 	return sum
 }
 
-func validOperation(result int, values []int) bool {
+func validOperation(part, result int, values []int) bool {
 	if len(values) == 0 || values[0] > result {
 		return false
 	}
@@ -46,65 +50,33 @@ func validOperation(result int, values []int) bool {
 	}
 
 	plus := make([]int, len(values))
-	mult := make([]int, len(values))
-
 	copy(plus, values)
-	copy(mult, values)
-
 	plus[1] += plus[0]
+
+	mult := make([]int, len(values))
+	copy(mult, values)
 	mult[1] *= mult[0]
 
-	return validOperation(result, plus[1:]) || validOperation(result, mult[1:])
+	if part == 2 {
+		conc := make([]int, len(values))
+		copy(conc, values)
+		conc[1] = concat(conc[0], conc[1])
+
+		return validOperation(part, result, plus[1:]) || validOperation(part, result, mult[1:]) || validOperation(part, result, conc[1:])
+	}
+	return validOperation(part, result, plus[1:]) || validOperation(part, result, mult[1:])
 }
 
 func (_ Day7) Run2(s *bufio.Scanner) int {
 	sum := 0
 	for s.Scan() {
-		line := s.Text()
-		resultsplit := strings.Split(line, ": ")
-		result, err := strconv.Atoi(resultsplit[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		numbers := strings.Split(resultsplit[1], " ")
-		values := make([]int, len(numbers))
-		for i, v := range numbers {
-			values[i], err = strconv.Atoi(v)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+		result, values := parseInput(s.Text())
 
-		if validOperation2(result, values) {
+		if validOperation(2, result, values) {
 			sum += result
 		}
 	}
-
 	return sum
-}
-
-func validOperation2(result int, values []int) bool {
-	if len(values) == 0 || values[0] > result {
-		return false
-	}
-
-	if len(values) == 1 {
-		return result == values[0]
-	}
-
-	plus := make([]int, len(values))
-	mult := make([]int, len(values))
-	conc := make([]int, len(values))
-
-	copy(plus, values)
-	copy(mult, values)
-	copy(conc, values)
-
-	plus[1] += plus[0]
-	mult[1] *= mult[0]
-	conc[1] = concat(conc[0], conc[1])
-
-	return validOperation2(result, plus[1:]) || validOperation2(result, mult[1:]) || validOperation2(result, conc[1:])
 }
 
 func concat(left, right int) int {

5e9c7e8bbe5b3056221f9b355c468758e0f0fdca
Marvi <accounts.marvi@mailbox.org>
2024-12-08 17:39:12 +0100

    prettify solution for day8

--- solutions/Day8/solution.go
+++ solutions/Day8/solution.go
@@ -1,6 +1,7 @@
 package day8
 
 import (
+	"AoC24/util"
 	"bufio"
 	"strings"
 )
@@ -13,34 +14,31 @@ type Point struct {
 }
 
 func (_ Day8) Run1(s *bufio.Scanner) int {
+	freqMap := createMap(s)
+	sizeX, sizeY := len(freqMap[0]), len(freqMap)
+
+	antennaPositions := getAntennaPositions(freqMap)
+	antiNodePositions := getAntiNodePositions(antennaPositions, sizeX, sizeY, 1)
+
+	return len(antiNodePositions)
+}
+
+func (_ Day8) Run2(s *bufio.Scanner) int {
+	freqMap := createMap(s)
+	sizeX, sizeY := len(freqMap[0]), len(freqMap)
+
+	antennaPositions := getAntennaPositions(freqMap)
+	antiNodePositions := getAntiNodePositions(antennaPositions, sizeX, sizeY, 2)
+
+	return len(antiNodePositions)
+}
+
+func createMap(s *bufio.Scanner) [][]string {
 	freqMap := make([][]string, 0)
 	for s.Scan() {
 		freqMap = append(freqMap, strings.Split(s.Text(), ""))
 	}
-	sizeX, sizeY := len(freqMap[0]), len(freqMap)
-
-	antennaPositions := getAntennaPositions(freqMap)
-
-	antiNodePositions := make(map[Point]bool)
-
-	for _, positions := range antennaPositions {
-		for i, position := range positions {
-			for j, other := range positions {
-				if i != j {
-					distanceX, distanceY := diff(position, other)
-					pos1, pos2 := Point{X: position.X + distanceX, Y: position.Y + distanceY}, Point{X: other.X - distanceX, Y: other.Y - distanceY}
-					if inBound(pos1, sizeX, sizeY) && !antiNodePositions[pos1] {
-						antiNodePositions[pos1] = true
-					}
-					if inBound(pos2, sizeX, sizeY) && !antiNodePositions[pos2] {
-						antiNodePositions[pos2] = true
-					}
-				}
-			}
-		}
-	}
-
-	return len(antiNodePositions)
+	return freqMap
 }
 
 func getAntennaPositions(freqMap [][]string) map[string][]Point {
@@ -49,13 +47,7 @@ func getAntennaPositions(freqMap [][]string) map[string][]Point {
 	for y, line := range freqMap {
 		for x, field := range line {
 			if field != "." {
-				a, ok := antennaPositions[field]
-				pos := Point{X: x, Y: y}
-				if ok {
-					antennaPositions[field] = append(a, pos)
-				} else {
-					antennaPositions[field] = []Point{pos}
-				}
+				antennaPositions[field] = append(antennaPositions[field], Point{X: x, Y: y})
 			}
 		}
 	}
@@ -71,56 +63,48 @@ func inBound(p Point, x, y int) bool {
 
 }
 
-func (_ Day8) Run2(s *bufio.Scanner) int {
-	freqMap := make([][]string, 0)
-	for s.Scan() {
-		freqMap = append(freqMap, strings.Split(s.Text(), ""))
-	}
-	sizeX, sizeY := len(freqMap[0]), len(freqMap)
-	antennaPositions := getAntennaPositions(freqMap)
+func getAntiNodePositions(antennaPositions map[string][]Point, sizeX, sizeY, part int) map[Point]bool {
 	antiNodePositions := make(map[Point]bool)
 
 	for _, positions := range antennaPositions {
 		for i, position := range positions {
 			for j, other := range positions {
-				if i != j {
-					distanceX, distanceY := diff(position, other)
+				if i == j {
+					continue
+				}
+				distanceX, distanceY := diff(position, other)
+				if part == 2 {
 					distanceX, distanceY = shorten(distanceX, distanceY)
-					pos1 := []Point{{X: position.X + distanceX, Y: position.Y + distanceY}}
-					pos2 := []Point{{X: other.X - distanceX, Y: other.Y - distanceY}}
+				}
+				pos1 := []Point{{X: position.X + distanceX, Y: position.Y + distanceY}}
+				pos2 := []Point{{X: other.X - distanceX, Y: other.Y - distanceY}}
+
+				if part == 2 {
 					for i := 0; inBound(pos1[i], sizeX, sizeY); i += 1 {
-						pos1 = append(pos1, Point{X: position.X + (i+1)*distanceX, Y: position.Y + (i+1)*distanceY})
+						pos1 = append(pos1, Point{X: position.X + i*distanceX, Y: position.Y + i*distanceY})
 					}
 					for i := 0; inBound(pos2[i], sizeX, sizeY); i += 1 {
-						pos2 = append(pos2, Point{X: position.X - (i+1)*distanceX, Y: position.Y - (i+1)*distanceY})
+						pos2 = append(pos2, Point{X: position.X - i*distanceX, Y: position.Y - i*distanceY})
 					}
+				}
 
-					for _, p := range pos1 {
-						if inBound(p, sizeX, sizeY) && !antiNodePositions[p] {
-							antiNodePositions[p] = true
-						}
+				for _, p := range pos1 {
+					if inBound(p, sizeX, sizeY) && !antiNodePositions[p] {
+						antiNodePositions[p] = true
 					}
-					for _, p := range pos2 {
-						if inBound(p, sizeX, sizeY) && !antiNodePositions[p] {
-							antiNodePositions[p] = true
-						}
+				}
+				for _, p := range pos2 {
+					if inBound(p, sizeX, sizeY) && !antiNodePositions[p] {
+						antiNodePositions[p] = true
 					}
 				}
 			}
 		}
 	}
-	return len(antiNodePositions)
+	return antiNodePositions
 }
 
 func shorten(x, y int) (int, int) {
-	gcd := gcd(x, y)
+	gcd := util.Gcd(x, y)
 	return x / gcd, y / gcd
 }
-
-func gcd(x, y int) int {
-	for y != 0 {
-		x, y = y, x%y
-	}
-	return x
-}
-
